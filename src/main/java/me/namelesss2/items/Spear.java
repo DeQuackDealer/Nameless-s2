cc9938c621bcd5cdda2f012c8d04cceecb991ebf
RileyDeQuackDealer <blazerlazar69@gmail.com>
2026-01-08 19:31:29 +0800

    Fix missing NamespacedKey import in Spear.java

--- src/main/java/me/namelesss2/items/Spear.java
+++ src/main/java/me/namelesss2/items/Spear.java
@@ -6,6 +6,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
