2172cc00e5e2298b0536cad3a287e10dda393d7d
RileyDeQuackDealer <blazerlazar69@gmail.com>
2026-01-09 10:29:08 +0800

    SpearListener: no changes needed

--- src/main/java/me/namelesss2/listeners/SpearListener.java
+++ src/main/java/me/namelesss2/listeners/SpearListener.java
@@ -1,11 +1,14 @@
 package me.namelesss2.listeners;
 
+import me.namelesss2.NamelessS2;
 import me.namelesss2.items.Spear;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -33,34 +36,52 @@ public class SpearListener implements Listener {
             return;
         }
 
+        FileConfiguration config = NamelessS2.getInstance().getConfig();
         Spear.Tier currentTier = Spear.getTier(spear);
         int currentKills = Spear.getKills(spear) + 1;
 
-        if (currentTier.getNextTier() != null && currentKills >= currentTier.getKillsToUpgrade()) {
+        int killsNeeded = getKillsToUpgrade(config, currentTier);
+
+        if (currentTier.getNextTier() != null && currentKills >= killsNeeded) {
             Spear.Tier newTier = currentTier.getNextTier();
             Spear.updateSpear(spear, newTier, 0);
 
-            killer.sendMessage(Component.text("Your spear has been upgraded to ")
+            String upgradeMessage = config.getString("messages.spear-upgrade", 
+                    "Your spear has been upgraded to %tier%!");
+            upgradeMessage = upgradeMessage.replace("%tier%", newTier.getDisplayName());
+
+            killer.sendMessage(Component.text(upgradeMessage)
                     .color(NamedTextColor.GREEN)
-                    .append(Component.text(newTier.getDisplayName())
-                            .color(newTier.getColor())
-                            .decoration(TextDecoration.BOLD, true))
-                    .append(Component.text("!")
-                            .color(NamedTextColor.GREEN)));
+                    .decoration(TextDecoration.BOLD, true));
 
             killer.playSound(killer.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, 1.0f);
+            killer.getWorld().spawnParticle(Particle.TOTEM_OF_UNDYING, 
+                    killer.getLocation().add(0, 1, 0), 50, 0.5, 0.5, 0.5, 0.1);
         } else {
             Spear.updateSpear(spear, currentTier, currentKills);
 
             if (currentTier.getNextTier() != null) {
-                killer.sendMessage(Component.text("Kill progress: ")
+                killer.sendMessage(Component.text("Spear Progress: ")
                         .color(NamedTextColor.GRAY)
-                        .append(Component.text(currentKills + "/" + currentTier.getKillsToUpgrade())
-                                .color(NamedTextColor.YELLOW)));
+                        .append(Component.text(currentKills + "/" + killsNeeded)
+                                .color(NamedTextColor.YELLOW)
+                                .decoration(TextDecoration.BOLD, true))
+                        .append(Component.text(" kills")
+                                .color(NamedTextColor.GRAY)));
             }
         }
 
         handleVictimSpearDowngrade(victim);
+    }
+
+    private int getKillsToUpgrade(FileConfiguration config, Spear.Tier tier) {
+        return switch (tier) {
+            case WOOD -> config.getInt("spear.wood.kills-to-upgrade", 1);
+            case COPPER -> config.getInt("spear.copper.kills-to-upgrade", 2);
+            case IRON -> config.getInt("spear.iron.kills-to-upgrade", 3);
+            case DIAMOND -> config.getInt("spear.diamond.kills-to-upgrade", 4);
+            case NETHERITE -> 999;
+        };
     }
 
     private void handleVictimSpearDowngrade(Player victim) {
@@ -83,7 +104,7 @@ public class SpearListener implements Listener {
                             .color(NamedTextColor.RED)));
         } else {
             Spear.updateSpear(spear, currentTier, 0);
-            victim.sendMessage(Component.text("Your kill progress has been reset!")
+            victim.sendMessage(Component.text("Your spear kill progress has been reset!")
                     .color(NamedTextColor.RED));
         }
     }
